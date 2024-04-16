fb76875ee96a4669a3392600494771dcfd1cbf5d
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2024-04-16 18:16:12 +0200

    Navigation and sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./Sidebar";
+
+export default function MobileSidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
